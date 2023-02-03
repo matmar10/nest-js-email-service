a2c92bfa73d3480221694f1fa2771358f75d27f0
Fahad Abdullah <fahad.abdullah@10pearls.com>
2023-02-03 15:04:23 +0500

    email service : email provider module and appcontroller tests are fixed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,34 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import EmailProviderService from './email-provider/email-provider.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private emailProviderService: EmailProviderService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('sendEmail')
-  async sendEmail(): Promise<any> {
-    return await this.emailProviderService
-      .sendMail({
-        to: 'fahad.abdullah@example.com',
-        subject: 'test email from service',
-        from: 'fahad.abdullah@example.com',
-        text: 'its email for testing dont take it seriously',
-        html: '<b>its email for testing dont take it seriously</b>', // HTML body content
-      })
-      .then(() => {
-        return 'email sent';
-      })
-      .catch(() => {
-        return 'error occured';
-      });
   }
 }
--- src/email-provider/email-provider.module.spec.ts
+++ src/email-provider/email-provider.module.spec.ts
@@ -1,19 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmailProviderModule } from './email-provider.module';
-import { SendGridService } from './send-grid/send-grid.service';
+import EmailProviderService from './email-provider.service';
 
-describe('EmailProviderModule', () => {
-  let module: EmailProviderModule;
+describe('EmailProviderService', () => {
+  const assertAll = (module: TestingModule) => {
+    const service = module.get<EmailProviderService>(EmailProviderService);
+    expect(service).toBeDefined();
+  };
 
-  beforeEach(async () => {
+  it('should provide default email provider (sync)', async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [EmailProviderModule],
+      imports: [
+        EmailProviderModule.forRoot({
+          transport: 'smtp://user@domain.com:pass@smtp@domain.com',
+        }),
+      ],
     }).compile();
-
-    const sendGrid = module.get<SendGridService>(SendGridService);
+    assertAll(module);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should provide default email provider (async)', async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        EmailProviderModule.forRootAsync({
+          useFactory: () => ({
+            transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+            defaults: {
+              from: '"nest-modules" <modules@nestjs.com>',
+            },
+          }),
+        }),
+      ],
+    }).compile();
+    assertAll(module);
   });
 });
